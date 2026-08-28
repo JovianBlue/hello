f6fa31f3612eabf49ac68e71b96a5c740a87275b
JovianBlue <keiraaadriance@gmail.com>
2026-08-28 15:00:28 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Keira Adriance
+ * Date: 2026/08/20
+ *
+ * A simple program that prints my name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int arcs, char **argv)
+{
+    printf("Keira Adriance!\n");
+    printf("Electrical Engineering\n");
+}
+
